2ceffc4ca620c140c852ba3de641ba956bae031a
77xiaoxian-a11y <77xiaoxian@gmail.com>
2026-07-19 17:37:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import os
 import requests
+import feedparser
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("CHAT_ID =", CHAT_ID)
+RSS = [
+    "https://feeds.bbci.co.uk/news/world/rss.xml",
+    "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+]
 
-r = requests.post(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": "🎉 GitHub 测试成功！"
-    }
-)
+for url in RSS:
+    feed = feedparser.parse(url)
 
-print("HTTP:", r.status_code)
-print(r.text)
+    if not feed.entries:
+        continue
+
+    news = feed.entries[0]
+
+    text = f"""📰 {news.title}
+
+🔗 {news.link}
+"""
+
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+    )
